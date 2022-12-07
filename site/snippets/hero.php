acb77b70346ce6e875799c3cfe4ba650356abe3a
Valentin de Brabandère <valentindebrabandere@gmail.com>
2022-12-07 11:31:09 +0100

    bcp de petites évolutions-backup avnt big changes

--- site/snippets/hero.php
+++ site/snippets/hero.php
@@ -1,17 +1,17 @@
 <header class="c-herobanner">
     <div class="c-herobanner__img">
-        <?php if ($image = $page->image()): ?>
-        <img class="o-fluidimage o-image-crop--right" src="<?= $image->url() ?>" alt="Hero banner">
+        <?php if ($image = $page->image()) : ?>
+            <img class="o-fluidimage o-image-crop--right" src="<?= $image->url() ?>" alt="Hero banner">
         <?php endif ?>
     </div>
     <div class=" c-herobanner__content">
         <h1 class="c-herobanner__title">
             <?= $page->headline()->or($page->title())->esc() ?>
         </h1>
-        <?php if ($page->subheadline()->isNotEmpty()): ?>
-        <h3 class="c-herobanner__headline">
-            <?= $page->subheadline()->esc() ?>
-        </h3>
+        <?php if ($page->subheadline()->isNotEmpty()) : ?>
+            <h3 class="c-herobanner__headline">
+                <?= $page->subheadline()->esc() ?>
+            </h3>
         <?php endif ?>
     </div>
 </header>
--- site/templates/actualites.php
+++ site/templates/actualites.php
@@ -1,10 +1,45 @@
+<?php
+function console_log($output, $with_script_tags = true) {
+    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
+');';
+    if ($with_script_tags) {
+        $js_code = '<script>' . $js_code . '</script>';
+    }
+    echo $js_code;
+}
+?>
+<?= console_log($page->children()->listed()->data()); ?>
+
+
 
 <?php snippet('header') ?>
 
 
-    <div class="o-container">
-      <?php snippet('layouts', ['field' => $page->layout()])  ?>
-  </div>
+<div class="o-container">
+
+<?php
+$listedElements = $page->children()->listed();
+?>
+
+<?php if($listedElements->count()): ?>
+    <ul>
+<?php endif; ?>
+
+    <?php foreach ($listedElements as $article): ?>
+
+    <li>
+    <h4><?= $article->title() ?></h4> 
+    <p><?= $article->subheadline() ?></p>
+    <img class="o-fluidimage" src="<?= $article->cover()->toFile()->url() ?>">  
+    </li>
+
+    <?php endforeach; ?>
+
+<?php if($listedElements->count()): ?>
+</ul>
+<?php endif; ?>
+
+</div>
 
 
 <?php snippet('footer') ?>
