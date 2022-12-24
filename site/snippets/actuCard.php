f808f2624dc9b30a5d9fdf25ba0de9ef5cfef007
Valentin de Brabandère <valentindebrabandere@gmail.com>
2022-12-24 19:05:30 +0100

    Joyeux Noel ! Légères rechtouches design dernier commit avant de fêter Noël

--- site/snippets/actuCard.php
+++ site/snippets/actuCard.php
@@ -5,18 +5,16 @@
                 <img class="o-fluidimage" src="<?= $item->cover()->toFile()->url() ?>">
     </div>
     <div class="c-actucard__body">
+        <p class="c-actucard__date"><?= $item->date()->toDate('d-m-Y') ?></p>
         <h3 class="c-actucard__title"><?= $item->headline() ?></h3> 
         <p class="c-actucard__tagline "><?= $item->subheadline() ?></p>
-        <div class="c-actucard__tag-date">
-            <ul class="c-actucard__tags">
-                <?php foreach ($item->tags()->split(',') as $tag): ?>
-                    <li class="c-actucard__tag-item">
-                        <p class="c-actucard__tag"><?= $tag ?></p>
-                    </li>
-                <?php endforeach ?>
-            </ul>
-            <p class="c-actucard__author-date"><?= $item->date()->toDate('d-m-Y') ?></p>
-        </div>
+        <ul class="c-actucard__tags">
+            <?php foreach ($item->tags()->split(',') as $tag): ?>
+                <li class="c-actucard__tag-item">
+                    <p class="c-actucard__tag"><?= $tag ?></p>
+                </li>
+            <?php endforeach ?>
+        </ul>
         <a href="<?= $item->url() ?>" class="c-actucard__link c-link">En savoir plus</a>
     </div>
 </article>
