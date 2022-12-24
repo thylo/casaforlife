9183b48b4f8da77e9400ea23d3b7b343eb16d32b
Valentin de Brabandère <valentindebrabandere@gmail.com>
2022-12-24 18:21:23 +0100

    grosses avancées sur actucards _ ajout de contenu // merci GPT chat

--- site/snippets/actuCard.php
+++ site/snippets/actuCard.php
@@ -1,24 +1,20 @@
 <?php $user = $item->author()->toUser()?>
 
 <article class="c-actucard">
-    <a href="<?= $item->url() ?>" class="c-actucard__link">
-        <div class="c-actucard__media">
-            <img class="o-fluidimage" src="<?= $item->cover()->toFile()->url() ?>">
-        </div>
-        <div class="c-actucard__body">
-            <h3 class="c-actucard__title"><?= $item->title() ?></h3> 
-            <p class="c-actucard__tagline "><?= $item->subheadline() ?></p>
-            <span class="c-actucard__author-date"><?= $item->date()->toDate('d-m-Y') ?></span>
-            <ul class="c-actucard__tags">
-                <?php foreach ($item->tags()->split(',') as $tag): ?>
-                    <li class="c-actucard__tag-item">
-                        <span class="c-actucard__tag"><?= $tag ?></span>
-                    </li>
-                <?php endforeach ?>
-            </ul>
-            <div class="actucard__author">
-                <p class="c-actucard__author-name"><?= $user->username() ?></p>
-            </div>
-        </div>
-    </a>
+    <div class="c-actucard__media">
+                <img class="o-fluidimage" src="<?= $item->cover()->toFile()->url() ?>">
+    </div>
+    <div class="c-actucard__body">
+        <h3 class="c-actucard__title"><?= $item->headline() ?></h3> 
+        <p class="c-actucard__tagline "><?= $item->subheadline() ?></p>
+        <span class="c-actucard__author-date"><?= $item->date()->toDate('d-m-Y') ?></span>
+        <ul class="c-actucard__tags">
+            <?php foreach ($item->tags()->split(',') as $tag): ?>
+                <li class="c-actucard__tag-item">
+                    <p class="c-actucard__tag"><?= $tag ?></p>
+                </li>
+            <?php endforeach ?>
+        </ul>
+        <a href="<?= $item->url() ?>" class="c-actucard__link">En savoir plus</a>
+    </div>
 </article>
--- site/templates/actualites.php
+++ site/templates/actualites.php
@@ -9,29 +9,29 @@
   </div>
 </div>
 
+<div class="c-pagesection">
+  <div class="l-container">
 
-<div class="l-container">
+  <?php
+  $listedElements = $page->children()->listed();
+  ?>
 
-<?php
-$listedElements = $page->children()->listed();
-?>
-
-<?php if($listedElements->count()): ?>
-    <ul class="l-grid l-grid--2cols@small l-grid--4cols@large">
-<?php endif; ?>
+  <?php if($listedElements->count()): ?>
+      <ul class="l-grid l-grid--2cols@small l-grid--4cols@large">
+  <?php endif; ?>
 
 
-<?php foreach ($listedElements as $item): ?>
-    <li>
-        <?= snippet('actuCard', ['item' => $item]) ?>
-    </li>
-<?php endforeach; ?>
+  <?php foreach ($listedElements as $item): ?>
+      <li>
+          <?= snippet('actuCard', ['item' => $item]) ?>
+      </li>
+  <?php endforeach; ?>
 
-<?php if($listedElements->count()): ?>
-</ul>
-<?php endif; ?>
+  <?php if($listedElements->count()): ?>
+  </ul>
+  <?php endif; ?>
 
+  </div>
 </div>
-
 
 <?php snippet('footer') ?>
