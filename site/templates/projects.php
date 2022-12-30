bfa8feb026f31c1ab93dfd0c99ff5a6e49ecad21
Valentin de Brabandère <valentindebrabandere@gmail.com>
2022-12-30 12:32:21 +0100

    mise en place de la page projects

---
+++ site/templates/projects.php
@@ -0,0 +1,37 @@
+
+<?php snippet('header') ?>
+
+<div class="c-pagesection c-pagesection--brand ">
+  <div class="l-container">
+    
+    <?php snippet('hero/hero') ?>
+
+  </div>
+</div>
+
+<div class="c-pagesection u-pt-m">
+  <div class="l-container">
+
+  <?php
+  $listedElements = $page->children()->listed()->sortBy('date','desc');
+  ?>
+
+  <?php if($listedElements->count()): ?>
+      <ul class="l-grid l-grid--2cols@small l-grid--3cols@large">
+  <?php endif; ?>
+
+
+  <?php foreach ($listedElements as $item): ?>
+      <li>
+          <?= snippet('actuCard', ['item' => $item]) ?>
+      </li>
+  <?php endforeach; ?>
+
+  <?php if($listedElements->count()): ?>
+  </ul>
+  <?php endif; ?>
+
+  </div>
+</div>
+
+<?php snippet('footer') ?>
