e8e7c89e5c639acea3b4ca22bb1503c72e4f5b7c
Valentin de Brabandère <valentindebrabandere@gmail.com>
2023-01-06 11:59:25 +0100

    wp - page contact

--- site/templates/contact.php
+++ site/templates/contact.php
@@ -12,6 +12,30 @@
   </div>
 </div>
 
+<?php function console_log($output, $with_script_tags = true) {
+$js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
+');';
+if ($with_script_tags) {
+    $js_code = '<script>' . $js_code . '</script>';
+}
+echo $js_code; } ?>
+
+  <div class="l-container">
+  <?php foreach ($page->equip() as $item): ?>
+    <?php foreach ($item): ?>
+
+    <div class="c-equip">
+      <div class="c-equip__img">
+        <img src="<?= $item->image()->toFile()->url() ?>" alt="">
+      </div>
+      <div class="c-equip__content">
+        <h2 class="c-equip__title"><?= $item->title() ?></h2>
+        <p class="c-equip__text"><?= $item->text() ?></p>
+      </div>
+    </div>
+  <?php endforeach ?>
+  </div>
+
 
   <div class="l-container">
     <?php foreach (page()->sections()->toBlocks() as $block): ?>
