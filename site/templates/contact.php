7246b11b521a7e3c16733d3950e51ff6a9c63d35
Valentin de Brabandère <valentindebrabandere@gmail.com>
2023-01-06 23:48:53 +0100

    equip section end

--- site/templates/contact.php
+++ site/templates/contact.php
@@ -16,18 +16,19 @@
   
   <div class="c-pagesection c-pagesection--notop">
     <div class="l-container">
-      <ul class="o-listbare l-grid l-grid--auto">
-        <?php foreach ($page->equipCard()->toBlocks() as $block): ?>
-            <?php snippet(
-              'blocks/' . $block->type(),
-              [
-                'block' => $block,
-                'theme' => 'dark'
-              ]
-            ) ?>
-          <?php endforeach ?>
-        </div>
-      </ul>
+        <h2 class="c-smalltitle"><?= $page->equipTitle() ?></h2>
+        <ul class="o-listbare l-grid l-grid--auto">
+          <?php foreach ($page->equipCard()->toBlocks() as $block): ?>
+              <?php snippet(
+                'blocks/' . $block->type(),
+                [
+                  'block' => $block,
+                  'theme' => 'dark'
+                ]
+              ) ?>
+            <?php endforeach ?>
+          </div>
+        </ul>
   </div>
 
 <?php endif ?>
