e40936256427729f3590fe1e4194b6d521793a7f
Julien Moreau <julien@thylo.be>
2023-03-05 11:14:49 +0100

    WIP Refactoring of the projects listing page

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -1,79 +1,53 @@
-
+<?php
+/** @var \Kirby\Cms\Page $page */
+$listedElements = $page->children()->listed()->sortBy('date', 'desc');
+$tag = param('tag');
+$projects = $page->children();
+?>
 <?php snippet('header') ?>
 
-<div class="c-pagesection c-pagesection--brand">
-  <div class="l-container">
-    
-    <?php snippet('hero/hero') ?>
+    <div class="c-pagesection c-pagesection--brand">
+        <div class="l-container">
 
-  </div>
-</div>
+            <?php snippet('hero/hero') ?>
 
-<div id="anchor-projects" class="c-pagesection u-pt-m">
-  <div class="l-container">
-
-  <?php
-  $listedElements = $page->children()->listed()->sortBy('date','desc');?>
-
-  <?php if($listedElements->isNotEmpty()): ?>
-    <div class="c-projects">
-      <ul class="c-projects__filters o-list-bare">
-        <li class="c-projects__filter-item">
-            <a href="<?= $page->url() ?>#anchor-projects" class="c-projects__filter 
-            <?= !isset($_GET['tag']) ? 'active' : '' ?>">Tous</a>
-        </li>
-      <?php foreach($page->children()->listed()->pluck('projectStatus', ',', true) as $tag): ?>
-        <li class="c-projects__filter-item">
-            <a href="<?= $page->url() . '?tag=' . $tag ?>#anchor-projects" class="c-projects__filter 
-            <?= isset($_GET['tag']) && $_GET['tag'] !== null && $tag === $_GET['tag'] ? 'active' : '' ?>"><?= $tag ?></a>
-        </li>
-      <?php endforeach?>
-      </ul>
-  <?php endif?>
-
-  <?php
-    $tag = $_GET['tag'] ?? null;
-    if($tag) {
-      $projects = $page->children()->listed()->sortBy('date','desc')->filterBy('projectStatus', $tag, ',');
-    } else {
-      $projects = $page->children()->listed()->sortBy('date','desc');
-    } ?>
-
-    <?php foreach($projects->pluck('projectType', ',', true) as $type): ?>
-      <?php
-        switch($type):
-          case 'agreculture':
-            $title = 'Nos réalisations en maraîchage et accès à l’eau';
-            break;
-          case 'handicap':
-            $title = 'Nos réalisations dans le domaine du handicap';
-            break;
-          case 'sante':
-            $title = 'Nos réalisations en santé et maternité';
-            break;
-          case 'autre':
-            $title = 'Autres réalisations';
-            break;
-        endswitch;
-      ?>
-        <div class="c-projects__section">
-          <h3 class="c-projects__title c-smalltitle"><?= $title ?></h3>
-            <?php if($projects->count()): ?>
-              <ul class="l-grid l-grid--2cols@small l-grid--3cols@large">
-            <?php endif; ?>
-
-                <?php foreach ($projects->filterBy('projectType', $type, ',') as $item): ?>
-                    <li>
-                        <?= snippet('projectCard', ['item' => $item]) ?>
-                    </li>
-                <?php endforeach; ?>
-
-              <?php if($listedElements->count()): ?>
-              </ul>
-              <?php endif; ?>
         </div>
-    <?php endforeach?>
-  </div>
-</div>
+    </div>
 
+    <div id="anchor-projects" class="c-pagesection u-pt-m">
+        <div class="l-container">
+            <?php if ($listedElements->isNotEmpty()): ?>
+            <div class="c-projects">
+                <ul class="c-projects__filters o-list-bare">
+                    <li class="c-projects__filter-item">
+                        <a href="<?= $page->url() ?>#anchor-projects" class="c-projects__filter
+            <?= !isset($tag) ? 'active' : '' ?>">Tous</a>
+                    </li>
+                    <?php foreach ($page->projectTypes()->toStructure() as $type): ?>
+                        <li class="c-projects__filter-item">
+                            <?= \Kirby\Toolkit\Html::a(url($page->url(), ['params' => ['tag' => $type->id()]]), $type->title(), [
+                                'class' => ['c-projects__filter', $tag == $type->id() ? 'active' : ""]
+                            ]) ?>
+                        </li>
+                    <?php endforeach ?>
+                </ul>
+                <?php endif ?>
+
+                <?php foreach ($page->projectTypes()->toStructure() as $type): ?>
+                    <div class="c-projects__section">
+                        <h3 class="c-projects__title c-smalltitle"><?= $type->title() ?></h3>
+                        <?php if ($projects->count()): ?>
+                            <ul class="l-grid l-grid--2cols@small l-grid--3cols@large">
+                                <?php foreach ($projects->filterBy('projectType', $type->id(), ',') as $item): ?>
+                                    <li>
+                                        <?= snippet('projectCard', ['item' => $item]) ?>
+                                    </li>
+                                <?php endforeach; ?>
+                            </ul>
+                        <?php endif; ?>
+                    </div>
+                <?php endforeach ?>
+            </div>
+        </div>
+    </div>
 <?php snippet('footer') ?>
